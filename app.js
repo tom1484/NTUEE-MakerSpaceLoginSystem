5b9efa1014cdbe092f68c0ffa354270c3ec51f80
tom1484 <angelus0918866416@gmail.com>
2022-02-24 09:38:14 +0800

    Update

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const express = require("express")
 const app = express()
 // allow parsing json object
 app.use(express.json())
-app.use("/mks_access", require("./record_updater/router"))
+app.use("/mks_access", require("./record_update/router"))
 
 app.listen(1484)
-
-
 
--- record_update/router.js
+++ record_update/router.js
@@ -12,11 +12,11 @@ mongoose.connect("mongodb://localhost:27017/mks_access")
 const router = express.Router()
 router.post("/record_update", async (req, res) => {
 	const post_data = req.body
-    // encrypt RFID with SHA-256
+	// encrypt RFID with SHA-256
 	post_data.RFID = crypto.createHash('sha256').update(post_data.RFID).digest('base64')
 	console.log(post_data)
 
-    // find corresponding member and then add one record
+	// find corresponding member and then add one record
 	await Member.findOne({ RFID: post_data.RFID })
 		.then((member) => {
 			console.log(member)
@@ -28,13 +28,13 @@ router.post("/record_update", async (req, res) => {
 			const record = new Record(record_data)
 			record.save()
 
-            // return successful execution result to ESP32
+			// return successful execution result to ESP32
 			res.json({
-				flag: 1, 
+				flag: "1", 
 				personalInfo: member, 
 			})
 		})
-		.catch(err => res.json({ flag: 0 }))
+		.catch(err => res.json({ flag: "0" }))
 })
 
 
